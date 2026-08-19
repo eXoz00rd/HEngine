ce28badf8c616994f6427e14cbee5be5d9052da8
Sebastian Rokicki <110182842+eXoz00rd@users.noreply.github.com>
2026-08-19 22:57:08 +0200

    Remove unused NetworkWriter/NetworkReader and INetworkSerializable (#58)

--- Src/Core/HEngine.Core/Contracts/INetworkSerializable.cs
+++
@@ -1,8 +0,0 @@
-﻿using HEngine.Core.Network;
-
-namespace HEngine.Core.Contracts;
-
-public interface INetworkSerializable {
-    void Serialize(NetworkWriter writer);
-    void Deserialize(NetworkReader reader);
-}
